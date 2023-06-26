8d0f7ee4bf238d98114eca7be1e5867cbae259c9
Sara <scalmunia@gmail.com>
2023-06-26 18:58:31 +0200

    instalacion mongodb

--- virtual-roleplay-game-back/src/app.ts
+++ virtual-roleplay-game-back/src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import path from "path";
+import { MongoClient } from "mongodb";
+import 'dotenv/config';
 
 import { loadApiEndpoints } from "./controllers/api";
 
 // Create Express server
 const app = express();
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
 
 // Express configuration
 app.set("port", process.env.PORT ?? 3000);
@@ -15,4 +20,7 @@ app.use(express.static(path.join(__dirname, "../public"), { maxAge: 31557600000 
 
 loadApiEndpoints(app);
 
+console.log(process.env.CONNECTION_MONGODB);
+
 export default app;
+
--- virtual-roleplay-game-back/src/controllers/api.ts
+++ virtual-roleplay-game-back/src/controllers/api.ts
@@ -3,7 +3,9 @@ import { Application, Request, Response } from "express";
 import CoursesData from "../../data/courses.json";
 
 export const loadApiEndpoints = (app: Application): void => {
+	//endpoint de prueba
 	app.get("/api", (req: Request, res: Response) => {
+		console.log('funcionaaaaa');
 		return res.status(200).send(CoursesData);
 	});
 };
