430ff6b74d024f8ffdafa1654725ba79df02e48d
Sara <scalmunia@gmail.com>
2023-06-30 11:28:31 +0200

    maquetacion editor de texto enriquecido

--- virtual-roleplay-game-front/src/app/app.module.ts
+++ virtual-roleplay-game-front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { QuillModule } from 'ngx-quill';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import { ProficiencyComponent } from './components/character-detail/proficiency/
   ],
   imports: [
     BrowserModule,
+    QuillModule,
     AppRoutingModule
   ],
   providers: [],
