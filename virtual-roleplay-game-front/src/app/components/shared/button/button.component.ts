9319af56b85e814ca2a054bb87da2d2ffcdc710a
Sara <scalmunia@gmail.com>
2023-07-21 18:04:37 +0200

    maquetar botones primario y secundario

--- virtual-roleplay-game-front/src/app/components/shared/button/button.component.ts
+++ virtual-roleplay-game-front/src/app/components/shared/button/button.component.ts
@@ -1,10 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'vrg-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnChanges {
   @Input() type: string = '';
+  @Input() color: string = '';
+
+  sideImageSrc: string = '';
+
+  ngOnChanges() {
+    this.sideImageSrc = this.color === 'primary' ? 'assets/images/pbutton_side.svg' : 'assets/images/sbutton_side.svg';
+  }
+
+  get colorClass(): string {
+    return this.color === 'primary' ? 'button-primary' : 'button-secondary';
+  }
 }
