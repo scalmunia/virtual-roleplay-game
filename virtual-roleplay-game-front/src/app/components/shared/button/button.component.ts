89942f0804f7f26d30af5265d051902efc5fd654
Sara <scalmunia@gmail.com>
2023-07-21 19:17:31 +0200

    refactor componente boton

--- virtual-roleplay-game-front/src/app/components/shared/button/button.component.ts
+++ virtual-roleplay-game-front/src/app/components/shared/button/button.component.ts
@@ -1,18 +1,30 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'vrg-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
+// export class ButtonComponent implements AfterViewInit {
 export class ButtonComponent implements OnChanges {
-  @Input() type: string = '';
-  @Input() color: string = '';
+  @Input() type: string = 'button';
+  @Input() color: 'primary' | 'secondary' = 'primary';
 
   sideImageSrc: string = '';
 
   ngOnChanges() {
-    this.sideImageSrc = this.color === 'primary' ? 'assets/images/pbutton_side.svg' : 'assets/images/sbutton_side.svg';
+    //El && evalua los términos uno a uno, y si todos los términos son 'truly' se queda con el último (un término 'falsy' sería, por ejermplo, '')
+    this.sideImageSrc =
+      (this.color === 'primary' && 'assets/images/pbutton_side.svg') ||
+      (this.color === 'secondary' && 'assets/images/sbutton_side.svg') ||
+      '';
+
+    // const images = {
+    //   primary: 'assets/images/pbutton_side.svg',
+    //   secondary: 'assets/images/sbutton_side.svg'
+    // }
+
+    // this.sideImageSrc = images[this.color];
   }
 
   get colorClass(): string {
