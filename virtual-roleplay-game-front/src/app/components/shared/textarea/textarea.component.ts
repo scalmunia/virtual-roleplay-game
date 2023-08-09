03c84b47562217f2386b937f24fd6e63cdf39ca6
Sara <scalmunia@gmail.com>
2023-08-09 11:57:02 +0200

    componente textarea hecho

---
+++ virtual-roleplay-game-front/src/app/components/shared/textarea/textarea.component.ts
@@ -0,0 +1,62 @@
+import { Component, Input, forwardRef, ViewChild, ElementRef } from '@angular/core';
+import { ControlValueAccessor, AbstractControl, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'vrg-textarea',
+  templateUrl: './textarea.component.html',
+  styleUrls: ['./textarea.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextareaComponent),
+      multi: true,
+    },
+  ],
+})
+
+export class TextareaComponent implements ControlValueAccessor {
+  @ViewChild('textarea', { static: false }) textarea?: ElementRef;
+
+  @Input() label: string = '';
+  @Input() id: string = '';
+  @Input() control: AbstractControl | null = null;
+  value: any = '';
+
+  // ControlValueAccessor methods
+  onChange: any = () => { };
+  onTouched: any = () => { };
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  // Emitir el evento valueChange cuando cambia el valor del option
+  onInputChange(event: Event): void {
+    const textarea = this.textarea?.nativeElement;
+    textarea.style.height = 'auto';
+    textarea.style.height = (textarea.scrollHeight) + 'px';
+
+    const value = (event.target as HTMLInputElement).value;
+    this.value = value;
+    this.control!.setValue(value);
+    this.onChange(value); // Notificar el cambio al formulario reactivo
+  }
+
+  getValue(): any {
+    return this.value;
+  }
+
+  // resizeTextarea() {
+  //   const textarea = this.textarea?.nativeElement;
+  //   textarea.style.height = 'auto';
+  //   textarea.style.height = (textarea.scrollHeight) + 'px';
+  // }
+}
