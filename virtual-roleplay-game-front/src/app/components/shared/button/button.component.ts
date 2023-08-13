faa7265d65f9cc59c771dc9257d2cb5d39a85233
asaldanam <asaldanam@gmail.com>
2023-08-13 21:09:31 +0200

    button refactor

--- virtual-roleplay-game-front/src/app/components/shared/button/button.component.ts
+++ virtual-roleplay-game-front/src/app/components/shared/button/button.component.ts
@@ -1,34 +1,15 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'vrg-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
-export class ButtonComponent implements OnChanges {
+export class ButtonComponent {
   @Input() type: string = 'button';
   @Input() color: 'primary' | 'secondary' | 'danger' | null = null;
-  @Input() variant: 'sghost' | 'dghost' | null = null;
+  @Input() variant: 'default' | 'ghost' = 'default';
 
-  sideImageSrc: string = '';
   backgroundColor: string = '';
-
-  ngOnChanges() {
-    //El && evalua los términos uno a uno, y si todos los términos son 'truly' se queda con el último (un término 'falsy' sería, por ejermplo, '')
-    this.sideImageSrc =
-      (this.color === 'primary' && 'assets/images/pbutton_side.svg') ||
-      (this.color === 'secondary' && 'assets/images/sbutton_side.svg') ||
-      (this.color === 'danger' && 'assets/images/dbutton_side.svg') ||
-      (this.variant === 'sghost' && 'assets/images/sgbutton_side.svg') ||
-      (this.variant === 'dghost' && 'assets/images/dgbutton_side.svg') ||
-      '';
-
-    // const images = {
-    //   primary: 'assets/images/pbutton_side.svg',
-    //   secondary: 'assets/images/sbutton_side.svg'
-    // }
-
-    // this.sideImageSrc = images[this.color];
-  }
 }
