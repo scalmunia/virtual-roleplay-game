d44377f4685847c056c6ff419e5881a7421cc34b
Sara <scalmunia@gmail.com>
2023-08-16 18:38:56 +0200

    modal del equipo terminado (sin imagen)

--- virtual-roleplay-game-front/src/app/components/character-detail/equipment-modal/equipment-modal.component.ts
+++ virtual-roleplay-game-front/src/app/components/character-detail/equipment-modal/equipment-modal.component.ts
@@ -1,25 +1,26 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  OnInit,
-} from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormArray, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+
+interface Attribute {
+  attribute: string;
+  bonus: string;
+  effect: string;
+  isAdded: boolean;
+}
 
 @Component({
   selector: 'vrg-equipment-modal',
   templateUrl: './equipment-modal.component.html',
   styleUrls: ['./equipment-modal.component.css'],
 })
+
 export class EquipmentModalComponent implements OnInit {
   form: FormGroup;
   attributes: FormArray;
   error: Error | null = null;
 
-  constructor(
-    private cd: ChangeDetectorRef,
-    public dialogRef: MatDialogRef<EquipmentModalComponent>
+  constructor(private cd: ChangeDetectorRef, public dialogRef: MatDialogRef<EquipmentModalComponent>
   ) {
     this.form = new FormGroup({
       name: new FormControl('', [Validators.maxLength(40)]),
@@ -27,43 +28,54 @@ export class EquipmentModalComponent implements OnInit {
       description: new FormControl('', [Validators.maxLength(256)]),
     });
 
-    const emptyAttribute = this.createAttributeForm();
-    this.attributes = new FormArray([emptyAttribute]);
+    this.attributes = new FormArray([
+      new FormGroup({
+        attribute: new FormControl(''),
+        bonus: new FormControl(''),
+        effect: new FormControl(''),
+        isAdded: new FormControl(false)
+      })
+    ]);
     // this.attributes = new FormArray([] as any[]);
   }
 
-  ngOnInit(): void {}
-
-  onSubmit() {
-    // Cambiar la clase del contenedor según la calidad seleccionada
-    const qualityValue = this.form.controls['quality'].value;
-
-    const result = {
-      // etc
-    };
-
-    console.log('attributes.value', this.attributes.value);
-
-    this.dialogRef.close(result);
-  }
+  ngOnInit(): void { }
 
   addAttribute() {
-    const emptyAttribute = this.createAttributeForm();
-    this.attributes.push(emptyAttribute);
+    const previousIndex = this.attributes.length - 1;
+
+    // Actualizar el estado isAdded para la fila anterior
+    if (previousIndex >= 0) {
+      const previousAttribute = this.attributes.at(previousIndex);
+      previousAttribute.get('isAdded')?.setValue(true);
+    }
+
+    // Agregar una nueva fila de atributos
+    this.attributes.push(
+      new FormGroup({
+        attribute: new FormControl(''),
+        bonus: new FormControl(''),
+        effect: new FormControl(''),
+        isAdded: new FormControl(false)
+      })
+    );
 
     // Workaround (en cristiano, ñapa): Fuerza la detección de cambios de angular una vez se ha limpiado la pila de llamadas
     setTimeout(() => this.cd.detectChanges());
   }
 
-  private createAttributeForm() {
-    return new FormGroup({
-      attribute: new FormControl(''),
-      bonus: new FormControl(''),
-      effect: new FormControl(''),
-    });
-  }
-
   removeAttribute(index: number) {
     this.attributes.removeAt(index);
+  }
+
+  onSubmit() {
+    const filteredAttributes = this.attributes.value.filter((attribute: Attribute) => attribute.isAdded);
+
+    const result = {
+      form: this.form.value,
+      attributes: filteredAttributes
+    };
+
+    this.dialogRef.close(result);
   }
 }
